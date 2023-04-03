88ca906e2bc550ef849425713be3ef0ab8392697
GTedya <gtedbest@gmail.com>
2023-04-03 17:49:27 +0300

    тест

---
+++ app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\PageResource\Pages;
+
+use App\Filament\Resources\PageResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPage extends EditRecord
+{
+    protected static string $resource = PageResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+
+    public function getTitle(): string
+    {
+        return 'Редактирование страницы';
+    }
+}
--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,11 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Carbon;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
+
+/**
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property ?string $content
+ * @property ?Carbon $created_at
+ * @property ?Carbon $updated_at
+ * @property ?Json $meta
+ * @property Collection<File> $files
+ * @property ?Media $media
+ */
 
 class Page extends Model implements HasMedia
 {
