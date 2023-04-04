f0b3676bc94e58f6c692f4e0a0059a718750f55c
GTedya <gtedbest@gmail.com>
2023-04-04 16:26:38 +0300

    Правки

---
+++ database/seeders/StatusSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $inserts = [
+            [
+                'id' => 1,
+                'name' => 'В обработке',
+            ],
+            [
+                'id' => 2,
+                'name' => 'В рассмотрении',
+            ],
+            [
+                'id' => 3,
+                'name' => 'Подтвержден',
+            ],
+        ];
+
+        DB::table('statuses')->insert($inserts);
+    }
+}
