59254f4ba4bdea4700efb0ec10002722fe2da326
GTedya <gtedbest@gmail.com>
2023-04-05 17:14:48 +0300

    Миграция и CRUD типов ТС

---
+++ app/Filament/Resources/VehicleTypeResource/Pages/EditVehicleType.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\VehicleTypeResource\Pages;
+
+use App\Filament\Resources\VehicleTypeResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditVehicleType extends EditRecord
+{
+    protected static string $resource = VehicleTypeResource::class;
+
+    protected function getActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'Редактирование типа';
+    }
+}
