47b75709af42b079111eaf581c3349fdda526605
GTedya <gtedbest@gmail.com>
2023-04-14 18:09:15 +0300

    Правки

--- app/Repositories/ManagerRepo.php
+++ app/Repositories/ManagerRepo.php
@@ -4,14 +4,14 @@ namespace App\Repositories;
 
 use App\Models\Order;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class ManagerRepo
 {
-    public function getOrders($userId): Collection
+    public function getOrders($userId): LengthAwarePaginator
     {
         return Order::query()->whereDoesntHave('banUsers', function (Builder $query) use ($userId) {
             $query->where('manager_order_bans.user_id', $userId);
-        })->get();
+        })->paginate();
     }
 }
