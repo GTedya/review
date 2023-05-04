87241b2284d64c307e7a07ce2bcb9e3eb6932e68
GTedya <gtedbest@gmail.com>
2023-05-04 17:07:35 +0300

    Api запрос списка типов ТС (в иерархии)

---
+++ app/Http/Controllers/VehTypeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\VehTypeResource;
+use App\Repositories\VehTypeRepo;
+use Illuminate\Http\JsonResponse;
+
+class VehTypeController extends Controller
+
+{
+    public VehTypeRepo $typeRepo;
+
+    public function __construct(VehTypeRepo $typeRepo)
+    {
+        $this->typeRepo = $typeRepo;
+    }
+
+
+    public function list(): JsonResponse
+    {
+        $types = $this->typeRepo->list();
+
+        $types = VehTypeResource::collection($types);
+
+        return response()->json(['success' => true, 'types' => $types]);
+    }
+}
