b32499ff89c379d3469c688bc15eb62eda432f65
GTedya <gtedbest@gmail.com>
2023-05-11 17:43:17 +0300

    правка

--- app/Repositories/ClaimRepo.php
+++ app/Repositories/ClaimRepo.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Collection;
 
 class ClaimRepo
 {
-    public function list(): Collection
-    {
-        return Geo::all();
-    }
-
     public function create($data): ?Claim
     {
         return Claim::create($data);
