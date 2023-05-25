08ec010d4bdbab494ebd786ae3992ace982297d2
GTedya <gtedbest@gmail.com>
2023-05-25 15:57:42 +0300

    Правка

--- app/Http/Resources/MenuItemResource.php
+++ app/Http/Resources/MenuItemResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class MenuResource extends JsonResource
+class MenuItemResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,11 +14,6 @@ class MenuResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'is_bottom' => $this->is_bottom,
-            'group_items' => MenuItemResource::collection($this->items),
-        ];
+        return parent::toArray($request);
     }
 }
