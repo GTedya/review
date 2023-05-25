750801c892765d28a6e6aa15280fe55f8323ba41
GTedya <gtedbest@gmail.com>
2023-05-25 15:53:17 +0300

    Правка

--- app/Models/MenuGroup.php
+++ app/Models/MenuGroup.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
@@ -10,9 +9,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * @property int $id
- * @property ?Carbon $deleted_at
  * @property string $name
- * @property ?string $link
  * @property bool $is_bottom
  * @property Collection<MenuItem> $items
  */
@@ -22,7 +19,6 @@ class MenuGroup extends Model
 
     protected $fillable = [
         'name',
-        'link',
         'is_bottom',
     ];
 
