7aae898ae6d80b0f619329196e509098890358eb
GTedya <gtedbest@gmail.com>
2023-06-08 12:21:14 +0300

    Наследование страниц

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Carbon;
 
@@ -11,6 +12,7 @@ use Illuminate\Support\Carbon;
  * @property int $id
  * @property string $title
  * @property string $slug
+ * @property ?int $parent_id
  * @property string $template
  * @property ?Carbon $created_at
  * @property ?Carbon $updated_at
@@ -35,6 +37,7 @@ class Page extends Model
 
     protected $fillable = [
         'title',
+        'parent_id',
         'template',
         'slug',
         'meta',
@@ -49,6 +52,16 @@ class Page extends Model
     public function getCreatedAtAttribute($value)
     {
         return $value;
+    }
+
+    public function parent(): BelongsTo
+    {
+        return $this->belongsTo(Page::class, 'parent_id');
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Page::class, 'parent_id');
     }
 
     public function pageVar(): hasOne
