3090d8fd9b8d5c405d5ddbb4fddb9ad0630f11be
GTedya <gtedbest@gmail.com>
2023-06-30 16:25:44 +0300

    правка

--- app/Http/Controllers/Manager/OrderController.php
+++ app/Http/Controllers/Manager/OrderController.php
@@ -9,11 +9,11 @@ use App\Repositories\ManagerRepo;
 use App\Services\OrderService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\ValidationException;
 
 class OrderController extends Controller
 {
-    public ManagerRepo $managerRepo;
+    private ManagerRepo $managerRepo;
+    private OrderService $orderService;
 
     public function __construct(ManagerRepo $managerRepo, OrderService $orderService)
     {
@@ -28,9 +28,6 @@ class OrderController extends Controller
         return response()->json(['success' => true, 'orders' => OrderResource::collection($orders)->resource]);
     }
 
-    /**
-     * @throws ValidationException
-     */
     public function takeOrder(int $orderId): JsonResponse
     {
         /** @var User $user */
