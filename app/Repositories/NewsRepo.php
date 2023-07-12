caa370dd7c30a0ebe5ad646d084e88f60aafa04b
GTedya <gtedbest@gmail.com>
2023-07-12 13:28:59 +0300

    Правка

--- app/Repositories/NewsRepo.php
+++ app/Repositories/NewsRepo.php
@@ -7,13 +7,15 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 class NewsRepo
 {
-    public function pagination(?int $perPage): LengthAwarePaginator
+    public function pagination(int $perPage): LengthAwarePaginator
     {
         return News::query()->orderBy('created_at', 'desc')->paginate($perPage);
     }
 
     public function single(string $slug): ?News
     {
-        return News::query()->where('slug', $slug)->with('files')->first();
+        /** @var ?News $news */
+        $news = News::query()->where('slug', $slug)->with('files')->first();
+        return $news;
     }
 }
